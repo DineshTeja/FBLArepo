2529d2faa1616ca5e896b73f45fac6a1786e6e03
DineshTeja <51058492+DineshTeja@users.noreply.github.com>
2019-11-27 17:10:08 -0600

    compEventsUpdate

--- FBLA_App/src/app/fbla-competitive-events/fbla-competitive-events.page.ts
+++ FBLA_App/src/app/fbla-competitive-events/fbla-competitive-events.page.ts
@@ -14,4 +14,53 @@ export class FBLACompetitiveEventsPage implements OnInit {
   ngOnInit() {
   }
 
+  openObjtest() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/objective-test/",'_system', 'location=yes');
+  }
+
+  openObjtestRole() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/objective-test-role-play/",'_system', 'location=yes');
+  }
+
+  openProdObj() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/production-objective-test/",'_system', 'location=yes');
+  }
+
+  openCollab() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/collaborative-objective-test-team-performance-role-play/",'_system', 'location=yes');
+  }
+
+  openPresWith() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/presentation-equipment/",'_system', 'location=yes');
+  }
+
+  openPresWithout() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/presentation-without-equipment/",'_system', 'location=yes');
+  }
+
+  openSpeech() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/speech/",'_system', 'location=yes');
+  }
+
+  openDemo() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/demonstration/",'_system', 'location=yes');
+  }
+
+  openPrePro() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/prejudged-projects-presentation/",'_system', 'location=yes');
+  }
+
+  openPreRep() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/prejudged-reports-presentation/",'_system', 'location=yes');
+  }
+
+  openInterview() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/interview/",'_system', 'location=yes');
+  }
+
+  openTeamPro() {
+    window.open("https://www.fbla-pbl.org/fbla/competitive-events/category/team-performance/",'_system', 'location=yes');
+  }
+
+
 }
